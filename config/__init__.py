245b8a2e438b4893a94ae1aa0d82cefe89dffb77
Loïc <b.loic38@laposte.net>
2021-04-04 14:08:55 -0400

    EDA started

--- config/__init__.py
+++ config/__init__.py
@@ -1,3 +1,6 @@
-from os.path import join
+from os.path import join, abspath, dirname
 
+DATA_DIRNAME = 'data'
+ROOT_DIR = abspath(dirname(__name__))
 
+DATA_PATH = join(ROOT_DIR, DATA_DIRNAME)
--- eda.py
+++ eda.py
@@ -1,3 +1,18 @@
+# # EDA (Exploratory Data Analysis)
+
 from torchvision.datasets import ImageFolder
+from config import *
+from PIL import Image
+import numpy as np
+
+# # Data loading
+
+dataloader = ImageFolder(DATA_PATH)
+
+dataloader
+
+img, target = next(iter(dataloader))
+
+img.convert('')
 
 
