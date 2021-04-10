e9e6480aa1ddb7663bd1c4fc446f7843f95b462d
Loïc <b.loic38@laposte.net>
2021-04-10 18:40:45 -0400

    Functions : OK

--- baseline.py
+++ baseline.py
@@ -9,14 +9,47 @@ from torchvision.models import resnet18
 import torch
 import torch.nn as nn
 from torch.utils.data import Subset, DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
 
 from sklearn.metrics import accuracy_score
+
+import numpy as np
 # -
 
 # # Data loading
 
-trans = transforms.ToTensor() # PIL to torch.Tensor
+# +
+# À définir manuellement
+train_rate = 0.8
+valid_rate = 0.1
+batch_size = 4
+
+trans = transforms.Compose([
+    transforms.Resize(size=(1500, 1500)),
+    transforms.ToTensor()
+])
 dataset = ImageFolder(DATA_PATH, transform=trans)
+
+train_size = int(train_rate * len(dataset))
+valid_size = int(valid_rate * len(dataset))
+
+# On split avec train-valid-test = 80-10-10
+all_indexes = np.arange(len(dataset))
+train_indexes = np.random.choice(all_indexes, size=train_size, replace=False)
+all_indexes = np.setdiff1d(all_indexes, train_indexes)
+valid_indexes = np.random.choice(all_indexes, size=valid_size, replace=False)
+test_indexes = np.setdiff1d(all_indexes, valid_indexes)
+
+train_subset = Subset(dataset, train_indexes)
+valid_subset = Subset(dataset, valid_indexes)
+test_subset = Subset(dataset, test_indexes)
+
+# Les dataloaders
+train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=False)
+valid_loader = DataLoader(valid_subset, batch_size=batch_size, shuffle=False)
+test_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=False)
+# -
 
 # # Baseline : Resnet-18
 
@@ -50,24 +83,28 @@ for name, param in model.named_parameters():
 
 # ## Entrainement
 
-# TODO : Ajouter le calcul de l’accuracy
 def train(model, optimizer, loss, train_loader, epochs=100, scheduler=None, valid_loader=None, gpu=None):
     # GPU
-    if gpu:
+    if gpu is not None:
         model = model.cuda(gpu)
 
     epochs_train_loss = []
     epochs_valid_loss = []
+    epochs_train_acc = []
+    epochs_valid_acc = []
     for ep in range(epochs):
-
+        model.training = True
+        
         all_losses = []
-        for inputs, targets in train_loader:
+        all_predictions = []
+        all_targets = []
+        for i, (inputs, targets) in enumerate(train_loader):
             # GPU
-            if gpu:
+            if gpu is not None:
                 inputs = inputs.cuda(gpu)
-                targets = targets.cuda(gpu)
-
-            predictions = model(inputs)
+                targets = targets.float().cuda(gpu)
+            
+            predictions = model(inputs).squeeze()
             err = loss(predictions, targets)
 
             # Machine is learning
@@ -75,21 +112,47 @@ def train(model, optimizer, loss, train_loader, epochs=100, scheduler=None, vali
             optimizer.step()
 
             # Clean GPU
-            if gpu:
-                err = err.cpu()
+            if gpu is not None:
+                err = err.detach().cpu()
                 inputs = inputs.cpu()
                 targets = targets.cpu()
+                predictions = predictions.cpu()
                 torch.cuda.empty_cache()
-
-            all_losses.append(err.detach().cpu())
-
+            
+            all_losses.append(err)
+            all_predictions.append((predictions >= 0.5) * 1)
+            all_targets.append(targets)
+            
+            print(f'\rBatch : {i+1} / {len(train_loader)}', end='')
+        
+        all_predictions = torch.hstack(all_predictions)
+        all_targets = torch.hstack(all_targets)
+        
+        train_loss = np.hstack(all_losses).mean()
+        train_acc = accuracy_score(all_targets, all_predictions)
+        
+        # Historique
+        epochs_train_acc.append(train_acc)
+        epochs_train_loss.append(train_loss)
+        
+        if scheduler is not None:
+            scheduler.step()
+        
         # Validation step
         if valid_loader is not None:
-            valid_losses = valid(model, loss, valid_loader, gpu)
-            print(f'\r', end='')
+            valid_loss, valid_acc = valid(model, loss, valid_loader, gpu)
+            # Historique
+            epochs_valid_acc.append(valid_acc)
+            epochs_valid_loss.append(valid_loss)
+            print(f'\rEpoch : {ep+1} - Train Accuracy : {train_acc*100:.2f}% - Train Loss : {train_loss:.2e} - Valid Accuracy : {valid_acc*100:.2f}% - Valid Loss : {valid_loss:.2e}')
         else:
             # Afficher les informations de l’époque
-            print(f'\r', end='')
+            print(f'\rEpoch : {ep+1} - Train Accuracy : {train_acc*100:.2f}%  - Train Loss : {train_loss:.2e}')
+        
+    if valid_loader is not None:
+        return epochs_train_acc, epochs_train_loss, epochs_valid_acc, epochs_valid_loss
+    
+    return epochs_train_acc, epochs_train_loss
 
 
 # ## Validation
@@ -98,24 +161,35 @@ def valid(model, loss, valid_loader, gpu):
     model.training = False
     with torch.no_grad():
         all_losses = []
+        all_predictions = []
+        all_targets = []
         for inputs, targets in valid_loader:
-            if gpu:
+            if gpu is not None:
                 inputs = inputs.cuda(gpu)
-                targets = targets.cuda(gpu)
+                targets = targets.float().cuda(gpu)
 
-            predictions = model(inputs)
+            predictions = model(inputs).squeeze()
             err = loss(predictions, targets)
 
             all_losses.append(err.detach().cpu())
 
             # Clean GPU
-            if gpu:
+            if gpu is not None:
                 err = err.cpu()
                 inputs = inputs.cpu()
                 targets = targets.cpu()
+                predictions = predictions.cpu()
                 torch.cuda.empty_cache()
-        all_losses = torch.vstack(all_losses)
-        return all_losses.mean()
+                
+            all_predictions.append((predictions >= 0.5) * 1)
+            all_targets.append(targets)
+        
+        all_losses = torch.hstack(all_losses)
+        all_predictions = torch.hstack(all_predictions)
+        all_targets = torch.hstack(all_targets)
+        valid_acc = accuracy_score(all_targets, all_predictions)
+        
+        return all_losses.mean(), valid_acc
 
 
 # ## Test
@@ -124,27 +198,63 @@ def test(model, loss, test_loader, gpu):
     model.training = False
     with torch.no_grad():
         all_losses = []
+        all_predictions = []
+        all_targets = []
         for inputs, targets in test_loader:
-            if gpu:
+            if gpu is not None:
                 inputs = inputs.cuda(gpu)
-                targets = targets.cuda(gpu)
+                targets = targets.float().cuda(gpu)
 
-            predictions = model(inputs)
+            predictions = model(inputs).squeeze()
             err = loss(predictions, targets)
 
             all_losses.append(err.detach().cpu())
 
             # Clean GPU
-            if gpu:
+            if gpu is not None:
                 err = err.cpu()
                 inputs = inputs.cpu()
                 targets = targets.cpu()
                 torch.cuda.empty_cache()
+                
+            all_predictions.append((predictions >= 0.5) * 1)
+            all_targets.append(targets)
+            
         all_losses = torch.vstack(all_losses)
-        return all_losses.mean()
+        all_predictions = torch.hstack(all_predictions)
+        all_targets = torch.hstack(all_targets)
+        test_acc = accuracy_score(all_targets, all_predictions)
+        
+        return all_losses.mean(), test_acc
 
 
 # ## Prédiction
 
-def predict():
-    pass
+# Input dim : (batch_size, chanels, height, width)
+def predict(model, tensor_data, gpu):
+    model.training = False
+    
+    if gpu is not None:
+        model = model.cuda(gpu)
+        tensor_data = tensor_data.cuda(gpu)
+    
+    with torch.no_grad():
+        predictions = model(tensor_data).squeeze()
+    return (predictions >= 0.5) * 1
+
+
+# # Entrainement
+
+# +
+learning_rate = 0.01
+
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+loss = nn.BCEWithLogitsLoss()
+
+# Test avec le test set (bizarre me direz vous ...)
+hist = train(model, optimizer, loss, train_loader=test_loader, epochs=100, gpu=0)
+# -
+
+# **Le réseau ne semble pas apprendre ...**
+
+
